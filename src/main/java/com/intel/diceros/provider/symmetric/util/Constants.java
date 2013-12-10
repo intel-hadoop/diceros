e5a7aa1dc1d65141df635eb74327cdfbda6b0c6c
dianfu <dian.fu@intel.com>
2013-12-10 18:58:28 +0800

    do some optimization for init in native code, reuse space of key and iv

---
+++ src/main/java/com/intel/diceros/provider/symmetric/util/Constants.java
@@ -0,0 +1,9 @@
+package com.intel.diceros.provider.symmetric.util;
+
+public class Constants {
+	public static final int MODE_CTR= 0;
+	public static final int MODE_CBC = 1;
+	
+	public static final int PADDING_NOPADDING = 0;
+	public static final int PADDING_PKCS5PADDING = 1;
+}
